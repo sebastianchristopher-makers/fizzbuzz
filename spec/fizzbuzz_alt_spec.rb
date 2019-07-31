453e45625d5feb2beff762a2c1e0a6e215327703
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-07-31 17:20:28 +0100

    added second failing test (RED) for fizzbuz_alt

--- spec/fizzbuzz_alt_spec.rb
+++ spec/fizzbuzz_alt_spec.rb
@@ -3,4 +3,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" for the number 3' do
     expect(3.fizzbuzz).to eq('fizz')
   end
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq('buzz')
+  end
 end
